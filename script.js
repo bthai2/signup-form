ac3c2d0cf3a4729ee94eb2cc3106e7a2f4abb25c
Brandon Thai <brandon88thai@hotmail.com>
2023-02-01 22:05:17 -0500

    Added all form elements

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const body = document.querySelector('body');
 const fieldset = document.querySelector('fieldset');
+const form = document.querySelector('form');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => form.submit());
 
 function addInput(node, type, id, placeholder, labelName){
     let label = document.createElement('label');
@@ -15,8 +19,9 @@ function addInput(node, type, id, placeholder, labelName){
     node.appendChild(input);
 }
 
-let fullName = document.createElement('div');
-fullName.classList.add('row');
+// User Name
+let nameInfo = document.createElement('div');
+nameInfo.classList.add('row');
 
 let firstName = document.createElement('div');
 firstName.classList.add('formElement');
@@ -26,7 +31,42 @@ let lastName = document.createElement('div');
 lastName.classList.add('formElement');
 addInput(lastName, 'text', 'last_name', 'Doe...', 'LAST NAME')
 
-fullName.appendChild(firstName);
-fullName.appendChild(lastName);
+nameInfo.appendChild(firstName);
+nameInfo.appendChild(lastName);
 
-fieldset.appendChild(fullName);
+fieldset.appendChild(nameInfo);
+
+// User Contact Info
+let contactInfo = document.createElement('div');
+contactInfo.classList.add('row');
+
+let email = document.createElement('div');
+email.classList.add('formElement');
+addInput(email, 'email', 'email_info', 'johndoe@example.com', 'EMAIL');
+
+let phone = document.createElement('div');
+phone.classList.add('formElement');
+addInput(phone, 'tel', 'phone_info', '123-456-7890', 'PHONE NUMBER');
+phone.lastChild.setAttribute('pattern', pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}');
+
+contactInfo.appendChild(email);
+contactInfo.appendChild(phone);
+
+fieldset.appendChild(contactInfo);
+
+// User Password
+let passwordInfo = document.createElement('div');
+passwordInfo.classList.add('row');
+
+let password = document.createElement('div');
+password.classList.add('formElement');
+addInput(password, 'password', 'pass_info', '', 'PASSWORD');
+
+let confirmPass = document.createElement('div');
+confirmPass.classList.add('formElement');
+addInput(confirmPass, 'password', 'confirm_pass', '', 'CONFIRM PASSWORD');
+
+passwordInfo.appendChild(password);
+passwordInfo.appendChild(confirmPass);
+
+fieldset.appendChild(passwordInfo);
